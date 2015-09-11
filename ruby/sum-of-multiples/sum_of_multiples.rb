8a45b2a0a2cb484f506284df0908c32102d7bfd1
Lafwind <lafwind@gmail.com>
2015-09-12 00:31:56 +0800

    solve ruby/sum-of-multiples

---
+++ ruby/sum-of-multiples/sum_of_multiples.rb
@@ -0,0 +1,31 @@
+class SumOfMultiples
+
+  def self.to(n)
+    [*1..n-1].select { |e| e % 3 == 0 || e % 5 == 0 }.reduce(0, :+)
+  end
+
+  def initialize(*args)
+    @args =  args
+  end
+
+  def to(n)
+    [*1..n-1].select do |e|
+      @args.inject(false) do |rst, a|
+        rst || e % a == 0
+      end
+    end.reduce(0, :+)
+  end
+
+  # Misunderstanding = =!
+
+  # def self.to(n)
+  #   [*1..n-1].select { |e| e == 1 || find_gcd(n, e) != 1 }.reduce(0, :+)
+  # end
+
+  # def self.find_gcd(a, b)
+  #   while a % b != 0
+  #     a, b = b, a % b
+  #   end
+  #   b
+  # end
+end
