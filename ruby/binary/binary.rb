82548f1b2767e470bf3bc750a65e882ca1c9a842
Lafwind <lafwind@gmail.com>
2015-10-05 19:23:41 +0800

    修改ruby/binary: 删掉不必要注释

--- ruby/binary/binary.rb
+++ ruby/binary/binary.rb
@@ -10,11 +10,5 @@ class Binary
     @str.chars.reverse.map.with_index do |ele, idx|
       ele.to_i * 2**idx
     end.reduce(:+)
-
-    # Version 1
-
-    # @str.chars.reverse.zip([*0..@str.length-1]).map do |e|
-    #   e.first.to_i * 2 ** e.last
-    # end.reduce(:+)
   end
 end
