596a2b3da4b6b215956ed710b02561df2d8ef5b5
Lafwind <lafwind@gmail.com>
2015-10-05 23:32:18 +0800

    修改ruby/grade_school: 使用三元運算符代替if...else...

--- ruby/grade-school/grade_school.rb
+++ ruby/grade-school/grade_school.rb
@@ -8,12 +8,8 @@ class School
   end
 
   def add(name, class_name)
-    @school[class_name] =
-      if @school[class_name].nil?
-        [name]
-      else
-        @school[class_name].push(name).sort{ |a, b| a <=> b }
-      end
+    @school[class_name] = @school[class_name].nil? ? [name] :
+                            @school[class_name].push(name).sort{ |a, b| a <=> b }
     @school = @school.to_a.sort { |a, b| a.first <=> b.first }.to_h
   end
 
