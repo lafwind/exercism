a64968018be676e39cc3f1357821c4d85198b219
Lafwind <lafwind@gmail.com>
2015-10-06 22:12:11 +0800

    修改ruby/space_age: 利用define_method消除重复代码

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -11,31 +11,23 @@ class SpaceAge
     (@secs.to_f / 60 / 60 / 24 / 365.25).round(2)
   end
 
-  def on_mercury
-    self.on_earth  / 0.2408467
+  StarTable = {
+    "mercury" => 0.2408467,
+    "venus" => 0.61519726,
+    "mars" => 1.8808158,
+    "jupiter" => 11.862615,
+    "saturn" => 29.447498,
+    "uranus" => 84.016846,
+    "neptune" => 164.79132
+  }
+
+  def self.calc_how_old_of(star)
+    define_method ("on_" + star).to_sym do
+      self.on_earth / StarTable[star]
+    end
   end
 
-  def on_venus
-    self.on_earth  / 0.61519726
-  end
-
-  def on_mars
-    self.on_earth / 1.8808158
-  end
-
-  def on_jupiter
-    self.on_earth / 11.862615
-  end
-
-  def on_saturn
-    self.on_earth / 29.447498
-  end
-
-  def on_uranus
-    self.on_earth / 84.016846
-  end
-
-  def on_neptune
-    self.on_earth / 164.79132
+  StarTable.keys.each do |star|
+    self.calc_how_old_of(star)
   end
 end
