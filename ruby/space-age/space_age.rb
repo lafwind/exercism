3d4cd8731b02fa19e3a9ab04bb51c550ac2c9e72
Lafwind <lafwind@gmail.com>
2015-10-06 22:28:16 +0800

    修改ruby/space_age: 采用method_missing消除重复代码

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -13,21 +13,18 @@ class SpaceAge
 
   StarTable = {
     "mercury" => 0.2408467,
-    "venus" => 0.61519726,
-    "mars" => 1.8808158,
+    "venus"   => 0.61519726,
+    "mars"    => 1.8808158,
     "jupiter" => 11.862615,
-    "saturn" => 29.447498,
-    "uranus" => 84.016846,
+    "saturn"  => 29.447498,
+    "uranus"  => 84.016846,
     "neptune" => 164.79132
   }
 
-  def self.calc_how_old_of(star)
-    define_method ("on_" + star).to_sym do
-      self.on_earth / StarTable[star]
+  def method_missing(name)
+    if StarTable.keys.include? (star = name.to_s.split('_').last)
+      return self.on_earth / StarTable[star]
     end
-  end
-
-  StarTable.keys.each do |star|
-    self.calc_how_old_of(star)
+    super
   end
 end
